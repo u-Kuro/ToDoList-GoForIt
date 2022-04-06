f11eae55943a03772da75a9acf37ac713beda236
u-Kuro <rcdupitas@gmail.com>
2022-04-06 17:07:15 +0800

    added a clearInterval for pinging host

--- app.js
+++ app.js
@@ -88,17 +88,18 @@ const ping = () => request('https://todolist-goforit.herokuapp.com/', (error, re
     console.log('statusCode:', response && response.statusCode);
     console.log('body:', body);
 });
-var offset = ((new Date().getTimezoneOffset())*-1)/60// Server Offset
+var offset = ((new Date().getTimezoneOffset())*-1)/60 // Server Offset
 var shouroffset = Math.floor(offset);
 var sminoffset = 60*(offset - shouroffset);
 var serverDate = new Date();
-serverDate.setHours(serverDate.getHours()-shouroffset+8);// Server to GMT+8 time
+serverDate.setHours(serverDate.getHours()-shouroffset+8); // Server to GMT+8 time
 serverDate.setMinutes(serverDate.getMinutes()-sminoffset);
 var localhour = serverDate.getHours();
-if (localhour >= 7 && localhour <= 21) {// 7am to 9pm
+if (localhour >= 7 && localhour <= 21) { // 7am to 9pm
     intervalisSet = true;
-    setInterval(ping, 5*60*1000); // Ping Website Interval 5 min
+    var pingweb = setInterval(ping, 5*60*1000); // Ping Website Interval 5 min
+} else {
+    clearInterval(pingweb);
 }
-
 
 module.exports = app;
