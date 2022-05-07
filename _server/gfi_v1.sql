4b524abfa77f541567faaa1397dfd676b7ae8148
u-Kuro <rcdupitas@gmail.com>
2022-05-07 23:22:42 +0800

    migrated to react frontend

--- _server/gfi_v1.sql
+++ _server/gfi_v1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 02, 2022 at 02:26 PM
+-- Generation Time: May 07, 2022 at 05:19 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.28
 
@@ -31,8 +31,17 @@ CREATE TABLE `category` (
   `id` int(5) NOT NULL,
   `users_id` int(5) NOT NULL,
   `category_name` varchar(255) NOT NULL,
-  `user_chosen` tinyint(1) NOT NULL
+  `recent_update` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `users_id`, `category_name`, `recent_update`) VALUES
+(185, 147, 'xxxcaCSfAC', '2022-05-06 21:34:13'),
+(188, 147, 'aASSASxXaxX', '2022-05-06 21:34:23'),
+(189, 147, 'CASasdCCDAADC', '2022-05-07 22:26:01');
 
 -- --------------------------------------------------------
 
@@ -45,6 +54,13 @@ CREATE TABLE `sessions` (
   `expires` int(11) UNSIGNED NOT NULL,
   `data` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sessions`
+--
+
+INSERT INTO `sessions` (`session_id`, `expires`, `data`) VALUES
+('KyJWwPc0FrPrv1oi4UGOS7F4ggUb00lR', 1652020065, '{\"cookie\":{\"originalMaxAge\":null,\"expires\":null,\"httpOnly\":true,\"path\":\"/\"},\"isAuth\":true,\"username\":\"123\",\"users_id\":147}');
 
 -- --------------------------------------------------------
 
@@ -61,8 +77,16 @@ CREATE TABLE `tasks` (
   `end_date` datetime NOT NULL,
   `description` varchar(255) NOT NULL,
   `date_status` varchar(255) NOT NULL,
-  `task_status` tinyint(1) NOT NULL
+  `taskisfinished` tinyint(1) NOT NULL,
+  `recent_update` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tasks`
+--
+
+INSERT INTO `tasks` (`id`, `category_id`, `users_id`, `task_name`, `start_date`, `end_date`, `description`, `date_status`, `taskisfinished`, `recent_update`) VALUES
+(192, 189, 147, 'adsad', '2022-05-07 21:57:00', '2022-05-07 22:00:00', 'asdsad', 'Missed', 0, '2022-05-07 21:59:10');
 
 -- --------------------------------------------------------
 
@@ -74,8 +98,16 @@ CREATE TABLE `users` (
   `id` int(5) NOT NULL,
   `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `recent_update` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `recent_update`) VALUES
+(147, '123', '123@gmail.com', '$2a$08$nwBAacv2iu6zHWltk0hAiui.eqgKecA2yNMRvRqWo3FimAgv9e.um', '0000-00-00 00:00:00');
 
 --
 -- Indexes for dumped tables
@@ -113,19 +145,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=190;
 
 --
 -- AUTO_INCREMENT for table `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=194;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=148;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
