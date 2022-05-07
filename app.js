613cd340e6c92f1999098298adb30e79a45bfbcb
u-Kuro <rcdupitas@gmail.com>
2022-05-07 22:56:19 +0800

    migrated to react frontend

--- app.js
+++ app.js
@@ -1,107 +1,106 @@
-const express = require('express');
-const path = require('path');
-var db = require('./db').db;
-const session = require('express-session');
-const MySQLStore = require('express-mysql-session')(session);
-require('dotenv').config();
+const express = require("express");
+const session = require("express-session");
+const MySQLStore = require("express-mysql-session")(session);
+const next = require("next");
+const path = require("path");
+const request = require("request");
+const db = require("./_server/db").db;
+require("dotenv").config();
 
-const app = express();
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const server = express();
+const handle = app.getRequestHandler();
 
-// Create Server
-let PORT = process.env.PORT || '5500'
-app.listen(PORT, () => {
-    console.log('Server started on port '+PORT);
-});
+app
+  .prepare()
+  .then(() => {
+    // Create Server
+    let PORT = process.env.PORT || "5500";
+    server.listen(PORT, (err) => {
+      if (err) throw err;
+      console.log("Server started on port " + PORT);
+    });
 
-const publicDirectory = path.join(__dirname, 'public');
+    // Set Directories
+    const publicdir = path.join(__dirname, "/_client/static");
 
-// // Auto Reload Template (HTML/HBS) Changes
-// const livereload = require('livereload');
-// const connectLivereload = require('connect-livereload');
-// const livereloadServer = livereload.createServer();
-// livereloadServer.watch(publicDirectory);
-// livereloadServer.server.once("connection", () => {
-//   setTimeout(() => {
-//     livereloadServer.refresh("/");
-//   }, 100);
-// });
-// app.use(connectLivereload());
+    // Use Static and Json
+    server.use(express.json());
+    server.use(express.urlencoded({ extended: false }));
+    server.use(express.static(publicdir));
 
-// view engine setup uses hbs instead of html file
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+    // Create Session (Always on Top of Routes)
+    var sessionStore = new MySQLStore(
+      {
+        host: process.env.database_host || "localhost",
+        port: process.env.database_port || "3306",
+        user: process.env.database_user || "root",
+        database: process.env.database || "gfi_v1",
+        clearExpired: true,
+        checkExpirationInterval: 900000,
+        expiration: 86400000,
+        createDatabaseTable: true,
+        connectionLimit: 10,
+        schema: {
+          tableName: "sessions",
+          columnNames: {
+            session_id: "session_id",
+            expires: "expires",
+            data: "data",
+          },
+        },
+      },
+      db
+    );
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(publicDirectory));
-app.use(express.static('public/icons'));
-app.use(express.static('public/scripts'));
+    server.use(
+      session({
+        name: process.env.sess_name || "zxcnjncabcajsbkadnckajdndhbkajnk",
+        secret: process.env.sess_secret || "dasjdaksdnsanfASFTEFkajdnknsadakj",
+        resave: false,
+        saveUninitialized: false,
+        store: sessionStore,
+      })
+    );
 
-// Create Session (Always on Top of Routes)
-var sessionStore = new MySQLStore({
-    host: process.env.database_host || 'localhost',
-    port: process.env.database_port || '3306',
-    user: process.env.database_user || 'root',
-    database: process.env.database || 'gfi_v1',
-    clearExpired: true,
-    checkExpirationInterval: 900000,
-    expiration: 86400000,
-    createDatabaseTable: true,
-    connectionLimit: 10,
-    schema: {
-		tableName: 'sessions',
-		columnNames: {
-			session_id: 'session_id',
-			expires: 'expires',
-			data: 'data'
-		}
-    }
-},db);
+    // Define Routes
+    server.use("/", require("./_server/api/pages"));
+    server.use("/auth", require("./_server/api/auth"));
+    server.use("/category", require("./_server/api/category"));
+    server.use("/task", require("./_server/api/task"));
 
-app.use(session({
-    name: process.env.sess_name || 'zxcnjncabcajsbkadnckajdndhbkajnk',
-	secret: process.env.sess_secret || 'dasjdaksdnsanfkajdnknsadakj',
-	resave: false,
-	saveUninitialized: false,
-    store: sessionStore
-}));
+    server.get("*", (req, res) => {
+      return handle(req, res);
+    });
 
-// Define Routes
-app.use('/', require('./routes/pages'));
-app.use('/auth', require('./routes/auth'));
-app.use('/category', require('./routes/category'));
-app.use('/task', require('./routes/task'));
+    // Check Database Connection
+    db.getConnection((error) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("MYSQL Connected...");
+      }
+    });
 
-// Check Database Connection
-db.getConnection((error) => {
-    if(error){
-        console.log(error)
-    } else {
-        console.log('MYSQL Connected...')
-    }
-});
+    // Keep The Server Running, Errors on Backend
+    process.on("uncaughtException", function (err) {
+      console.log(err);
+    });
 
-// Keep The Server Running, Errors on Backend
-process.on('uncaughtException', function (err) {
-    console.log(err);
-}); 
-
-// Ping Deployed App
-const request = require('request');
-const ping = () => request('https://todolist-goforit.herokuapp.com/', (error, response, body) => {});
-var pingweb = setInterval(function () {
-    var offset = ((new Date().getTimezoneOffset())*-1)/60 // Server Offset
-    var shouroffset = Math.floor(offset);
-    var sminoffset = 60*(offset - shouroffset);
-    var serverDate = new Date();
-    serverDate.setHours(serverDate.getHours()-shouroffset+8); // Server to GMT+8 PH
-    serverDate.setMinutes(serverDate.getMinutes()-sminoffset);
-    var localhour = serverDate.getHours();
-    if (localhour >= 7 && localhour <= 20) { // Ping between 7am to 9pm
-        ping;
-    } else {
-        clearInterval(pingweb);
-    }
-}, 5*60*1000); // Ping Website every 5 min
+    // Ping Deployed App
+    const ping = () =>
+      request(
+        "https://todolist-goforit.herokuapp.com/",
+        (error, response, body) => {}
+      );
+    setInterval(function () {
+      ping;
+    }, 60 * 1000); // Ping Website every 1 min
+  })
+  .catch((ex) => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
 
 module.exports = app;
