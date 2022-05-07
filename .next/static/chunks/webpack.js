9d8a3c0d28afaecd2377b6eb73f97f0f1debbcbb
u-Kuro <rcdupitas@gmail.com>
2022-05-08 02:53:33 +0800

    migrated to react frontend

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "25b35931f1b7c8bf"; }
+/******/ 		__webpack_require__.h = function() { return "7e715260fab2cbb1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app
     });
 
     // Set Directories
-    const publicdir = path.join(__dirname, "/_client/static");
+    const publicdir = path.join(__dirname, "/public/static");
 
     // Use Static and Json
     server.use(express.json());
