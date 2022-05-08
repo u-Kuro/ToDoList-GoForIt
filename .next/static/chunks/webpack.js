e5cd71da7f21b6a4761b20aa7bb7d66fa9569b34
u-Kuro <rcdupitas@gmail.com>
2022-05-08 11:04:20 +0800

    migrated to react frontend

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3c7a206cacb8b96a"; }
+/******/ 		__webpack_require__.h = function() { return "dd9fcd8875206abf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.e7a93ba5012a1486.hot-update.js
+++ .next/static/webpack/webpack.e7a93ba5012a1486.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "55fa17f3b979f566"; }
+/******/ 	__webpack_require__.h = function() { return "dd9fcd8875206abf"; }
 /******/ }();
 /******/ 
 /******/ }
