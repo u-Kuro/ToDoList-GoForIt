055d8f525adf691523268814ddcb9e7e95f4f400
u-Kuro <102844452+u-Kuro@users.noreply.github.com>
2022-05-20 10:34:25 +0800

    edited getter and setter, all are still not added

--- helpers/animation.js
+++ helpers/animation.js
@@ -1,17 +1,24 @@
-// const DOM = (element) => {
+// const DOM = (element_s) => {
      if(typeof id==="string" || id instancof String)
-       new Elements[...document.querySelectorAll(element)])
-     else new Elements([element])
+       new Elements[...document.querySelectorAll(element_s)])
+     else new Elements([element_s])
 // }
 
 // class Elements extends Array {
+    
+     isElement
+     constructor(elements) {
+       this.isElement = elements.length===1
+       this.element = elements[0]
+     }
+
 //   //Listener
-     ready(callback){
+     ready(callback) {
        if(this.some(element=>element.readyState!==null && element.readyState!=="loading"))
          callback()
        else this.on("DOMContentLoaded,callback))
      }
-//   on(event,callback_element,callback){
+//   on(event,callback_element,callback) {
        if(typeof callback_element==="function")
          this.forEach(element=>element.addEventListener(event,callback))
        else this.forEach(element=>element.addEventListener(event,e=>{
@@ -19,25 +26,25 @@
        })
 //   }
 //   //Animate
-//   css(properties_values,values){
+//   css(properties_values,values) {
        const _properties = typeof properties_values==="object"? Object.keys(properties_values):properties_values
        const _values = typeof properties_values==="object"? Object.values(properties_values):typeof values==="string"||values instanceof String? values:""
-       if(_values.length) return this.forEach(element=>element.style[_properties])
+       if(_values.length) return this.map(element=>element.style[_properties])
        else {
          this.forEach(element=>element.style[_properties]=_values)
          return this
        }
      }
-    animate(keyframe, duration_callback, easing_iterations_callback, iterations_callback, callback){
+    animate(keyframe, duration_callback, easing_iterations_callback, iterations_callback, callback) {
       const _duration = typeof duration_callback==="function"? 0:duration_callback
       const _easing = typeof easing_callback==="function"? "cubic-bezier(0, 0, 0.5, 1.25)":easing_callback
       const _iterations = typeof iterations_callback==="function"? 1:iteration_callback
       const _callback = typeof callback==="function"? callback:()=>{}
-      this.forEach(element=>{
-        element.animate([{},keyframe],{
+      this.forEach(element=> {
+        element.animate([{},keyframe], {
           duration:_duration, fill:"forwards", easing:_easing, iterations:_iterations})
       })
-      setTimeout(()=>{
+      setTimeout(()=> {
         _callback()
         return this
       },_duration)    
@@ -47,59 +54,83 @@
 //     this.forEach(element=>element.setCustomValidity(message))
 //     return this
 //   }
-//   reportValidity() {
-//     this.element.reportValidity()
+//   get reportValidity(){
+//     this.forEach(element=>element.reportValidity())
 //     return this
 //   }
-//   // Values
+//   // Setter
 //   val(value) {
-//     this.
-//     else return this.element.value = value
+//     this.forEach(element=>element.value=value)
+       return this
 //   }
-//   show() {
-//     this.element.style.display = "block"
+//   get show() {
+//     this.forEach(element=>element.style.display = "block")
 //     return this
 //   }
-//   hide = () => {
-//     this.element.style.display = "none"
+//   get hide() {
+//     this.forEach(element=>element.style.display="none")
 //     return this
 //   }
-//   height = () => {
-//     return this.element.style.height
+     // Getter
+//   get height() {
+//     return (this.isElement? this.element.style.height
+         : this.map(element=>element.style.height)
+       )
 //   }
-//   width = () => {
-//     return this.element.style.width
+//   get width() {
+//     return (this.isElement? this.element.style.width
+         : this.map(element=>element.style.width)
+       )
 //   }
 //   //
-//   offsetTop = () => {
-//     return this.element.offsetTop
+//   get offsetTop() {
+//     return (this.isElement? this.element.offsetTop
+         : this.map(element=>element.offsetTop)
+       )
 //   }
-//   offsetBottom = () => {
-//     return this.element.offsetBottom
+//   get offsetBottom() {
+//     return (this.isElement? this.element.offsetBottom
+         : this.map(element=>element.offsetBottom)
+       )
 //   }
-//   offsetRight = () => {
-//     return this.element.offsetRight
+//   get offsetRight() {
+//     return (this.isElement? this.element.offsetRight
+         : this.map(element=>element.offsetRight)
+       )
 //   }
-//   offsetLeft = () => {
-//     return this.element.offsetLeft
+//   get offsetLeft() {
+//     return (this.isElement? this.element.offsetLeft
+         : this.map(element=>element.offsetLeft)
+       )
 //   }
-//   innerWidth = () => {
-//     return this.element.innerWidth
+//   get innerWidth() {
+//     return (this.isElement? this.element.innerWidth
+         : this.map(element=>element.innerWidth)
+       )
 //   }
-//   outerWidth = () => {
-//     return this.element.outerWidth
+//   get outerWidth() {
+//     return (this.isElement? this.element.outerWidth
+         : this.map(element=>element.outerWidth)
+       )
 //   }
-//   innerHeight = () => {
-//     return this.element.innerHeight
+//   get innerHeight() {
+//     return (this.isElement? this.element.innerHeight
+         : this.map(element=>element.innerHeight)
+       )
 //   }
-//   outerHeight = () => {
-//     return this.element.outerHeight
+//   get outerHeight() {
+//     return (this.isElement? this.element.outerHeight
+         : this.map(element=>element.outerHeight)
+       )
 //   }
-//   scrollTop = (length) => {
-//     return (typeof length==="undefined"?this.element.scrollTop:this.element.scrollTop = length)
+//   get scrollTop() {
+//     return (this.isElement? this.element.scrollTop
+         : this.map(element=>element.scrollTop)
 //   }
-//   isActive = () => {
-//     return this.element === document.activeElement
+//   get isActive() {
+//     return (this.isElement? this.element === document.activeElement)
+         : this.map(element=>element === document.activeElement)
+       )
 //   }
 // }
 
